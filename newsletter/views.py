6151b58b5f5ca48f74b28b1fd568ae0384e08ec1
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-12-12 00:00:03 +0300

    add count

--- newsletter/views.py
+++ newsletter/views.py
@@ -7,10 +7,23 @@ from newsletter.models import Client
 
 class IndexView(TemplateView):
     template_name = 'newsletter/index.html'
+    model = Client
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data()
+        count = Client.objects.count()
+        context_data['count'] = count
+        return context_data
 
 
 class ClientListView(ListView):
     model = Client
+
+    # def get_context_data(self, **kwargs):
+    #     context_data = super().get_context_data()
+    #     count = 0
+    #     context_data['count'] = count + 1
+    #     return context_data
 
 
 class ClientCreateView(CreateView):
